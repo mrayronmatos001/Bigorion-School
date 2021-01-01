d2e772e1a00ba1d2b5ec1ae80614809cc606d66e
Ayron Matos <mrayronmatos01@gmail.com>
2021-01-01 11:49:52 -0300

    add index change 3

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 // CONTADOR DE VISITANTES
-if(typeof(Storage)!="undefine"){
-    if(localStorage.visitas){
-        localStorage.visitas=Number(localStorage.visitas)+1
-    }else{
-        localStorage.visitas=1;
-    } 
-}else{
-    document.write("Sem suporte a Web Storage!");
-}
+// if(typeof(Storage)!="undefine"){
+//     if(localStorage.visitas){
+//         localStorage.visitas=Number(localStorage.visitas)+1
+//     }else{
+//         localStorage.visitas=1;
+//     } 
+// }else{
+//     document.write("Sem suporte a Web Storage!");
+// }
 
-const container = document.querySelector('#container_idioma');
-const img = document.querySelector('#mapa_main');
-// const paragrafo =  document.querySelector('#container_idioma>p');
+const container = document.querySelector('nav');
+const lista = document.querySelector('ul');
+const img = document.querySelector('nav>img');
 
 container.onmouseover = function() {
     img.src = "img/mapa2.svg"
-    // paragrafo.style.color = "cyan"
 }
 
 container.onmouseout = function() {
-    img.src = "img/mapa.svg"
-    // paragrafo.style.color = "white"
+    if (lista.classList.contains('caixa')) { 
+        img.src = "img/mapa2.svg"
+    } else {
+        img.src = "img/mapa.svg"
+    }
+}
+    
+container.onclick = function() {
+    lista.classList.toggle('ativa');
+    container.classList.toggle('caixa');
 }

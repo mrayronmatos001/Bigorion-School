f4aea632dabe27e7f2fb10f5d0b51998b73d5d5c
Ayron Matos <mrayronmatos01@gmail.com>
2021-01-02 12:53:20 -0300

    add index change 7

--- script.js
+++ script.js
@@ -12,9 +12,16 @@
 const container = document.querySelector('nav');
 const lista = document.querySelector('ul');
 const img = document.querySelector('nav>img');
+const dropdown = [container, lista, img];
+
+img.onmouseover = function() {
+  img.src = "img/mapa2.svg"
+}
 
 container.onmouseover = function() {
-    img.src = "img/mapa2.svg"
+    if(container.classList.contains('caixa')) {
+      img.src = "img/mapa2.svg"
+    }
 }
 
 container.onmouseout = function() {
@@ -25,7 +32,17 @@ container.onmouseout = function() {
     }
 }
 
-container.onclick = function() {
+img.onclick = function() {
     lista.classList.toggle('ativa');
     container.classList.toggle('caixa');
 }
+
+window.addEventListener('click', function(event) {
+  const {target} = event;
+  const isClickeItemAdropdownPart = dropdown.some(e =>  e === target)
+
+  if(!isClickeItemAdropdownPart) {
+    lista.classList.remove('ativa');
+    container.classList.remove('caixa');
+  }
+});

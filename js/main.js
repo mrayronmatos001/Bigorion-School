bc2c7fe54b0f71c6ecf496824cd165432b0d107a
Bigorion School <74272557+mrayronmatos001@users.noreply.github.com>
2021-02-23 13:57:56 -0300

    problema na animação de entrada dos botões resolvido

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 const h1 = document.querySelector('h1')
-const btn1 = document.querySelector('#btn1')
-const btn2 = document.querySelector('#btn2')
+const btn1 = document.querySelector('div#btn1')
+const btn2 = document.querySelector('div#btn2')
 function textFloat() {
   setTimeout(function(){h1.classList.add("slide")},0)
-  setTimeout(function(){btn1.classList.add("slide2")},250)
-  setTimeout(function(){btn2.classList.add("slide3")},500)
+  setTimeout(function(){
+    btn1.classList.add("slide2")
+    setTimeout(function(){btn1.classList.add("opacidade")},1300)
+  },250)
+  setTimeout(function(){
+    btn2.classList.add("slide3")
+    setTimeout(function(){btn2.classList.add("opacidade")},1300)
+  },500)
 }
